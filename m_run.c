ea7908182bf4bdb720d75fdfd0afe7475f6dc834
Obarechrispus <obarechrispus@gmail.com>
2024-02-27 23:59:54 -0800

    task1

--- m_run.c
+++
@@ -1,6 +0,0 @@
-#include "run.h"
-
-int main(void) {
-    run_shell();
-    return 0;
-}
--- run.h
+++
@@ -1,7 +0,0 @@
-#ifndef RUN_H
-#define RUN_H
-
-/* Function prototypes*/
-void run_shell(void);
-
-#endif /* RUN_H */
